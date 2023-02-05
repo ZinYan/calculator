47a6fb13b7c8bdce7c6da422d33d9251acea38f2
ZinYan <zinwaiyan2226@gmail.com>
2023-02-05 19:49:03 +0630

    new operation

--- calculator.py
+++ calculator.py
@@ -10,5 +10,7 @@ def calculator():
         print(num1*num2)
     elif op == '/':
         print(num1/num2)
+     elif op=='%':
+        print(num1%num2)
     else:
-        print('Invalid operator')
+        print('Invalid operator!!')
