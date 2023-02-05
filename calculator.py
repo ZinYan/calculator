72d0f253c8f69f60fc07719e85940405de4606d3
ZinYan <zinwaiyan2226@gmail.com>
2023-02-05 19:40:28 +0630

    First Commit

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def calculator():
+    num1=float(input("Enter 1st number:"))
+    op=input('Enter operator:')
+    num2=float(input("Enter 2nd number:"))
+    if op=='+':
+        print(num1+num2)
+    elif op=='-':
+        print(num1-num2)
+    elif op=='*':
+        print(num1*num2)
+    elif op=='/':
+        print(num1/num2)
+    elif op=='%':
+        print(num1%num2)
+    else:
+        print('Invalid operator')
